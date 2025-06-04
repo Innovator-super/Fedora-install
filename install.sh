aa183ea9d75bc6e6b01cca970a718f75616f807d
Петров Алексей Дмитриевич <alex@192.168.1.13>
2025-06-04 10:19:10 +0300

    first commit

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+sudo dnf update -y
+
+sudo dnf install vim gparted clapper kitty -y
+
+sudo dnf install dnf-plugins-core -y
+
+sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo -y
+
+sudo dnf install brave-browser -y
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+sudo dnf install google-chrome-stable_current_x86_64.rpm -y
+
+flatpak install flathub com.obsproject.Studio md.obsidian.Obsidian com.github.IsmaelMartinez.teams_for_linux org.gimp.GIMP org.gnome.gitlab.YaLTeR.VideoTrimmer com.github.unrud.VideoDownloader org.pgadmin.pgadmin4 us.zoom.Zoom com.mattjakeman.ExtensionManager com.visualstudio.code org.telegram.desktop -y
+
+cat rm.txt >> ~/.bash_profile
+cat "alias.txt" >> ~/.bashrc
+source ~/.bashrc
+
+read -p "Do you want to restart (y or n)? " flag
+
+if [ "$flag" = "y" ]; then
+	reboot
+fi
