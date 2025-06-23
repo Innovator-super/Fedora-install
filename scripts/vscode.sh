db30963b6d9f44075f494dde078171ec45d5495d
Alexey <alex@192.168.1.13>
2025-06-23 16:57:08 +0300

    fourth commit

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp ..files/settings.json ~/.var/app/com.visualstudio.code/config/Code/User/
+cp files/settings.json ~/.var/app/com.visualstudio.code/config/Code/User/
 wget https://download-cdn.jetbrains.com/fonts/JetBrainsMono-2.304.zip
 unzip JetBrainsMono-2.304.zip -d JetBrainsMono-2.304
 mv JetBrainsMono-2.304 ~/.local/share/fonts/
