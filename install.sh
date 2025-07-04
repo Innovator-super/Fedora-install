25d9c8521efc246b3ea9a34f13fa8f3f8aea15ab
Alexey <alex@192.168.1.11>
2025-07-04 22:02:55 +0300

    sixth commit

--- install.sh
+++ install.sh
@@ -13,6 +13,9 @@ sudo dnf install dnf-plugins-core -y
 
 sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo -y
 
+sudo dnf install snapd -y
+sudo ln -s /var/lib/snapd/snap /snap
+
 sudo dnf install brave-browser -y
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
@@ -21,10 +24,19 @@ sudo dnf install google-chrome-stable_current_x86_64.rpm -y
 flatpak install flathub com.obsproject.Studio md.obsidian.Obsidian com.github.IsmaelMartinez.teams_for_linux org.gimp.GIMP org.gnome.gitlab.YaLTeR.VideoTrimmer com.github.unrud.VideoDownloader org.pgadmin.pgadmin4 us.zoom.Zoom com.mattjakeman.ExtensionManager com.visualstudio.code org.telegram.desktop -y
 
 bash scripts/vscode.sh
+bash scripts/oh-my-posh.sh
 
 cat files/rm.txt >> ~/.bash_profile
 cat files/"alias.txt" >> ~/.bashrc
+cat files/config.jsonc ~/.config/fastfetch/config.jsonc
+cat files/bashrc >> ~/.bashrc
 source ~/.bashrc
+
+read -p "Do you want watch anime (y or n)? " flag
+
+if [ "$flag" = "y" ]; then
+    sudo snap install anilibrix
+fi
 
 read -p "Do you want to set a keyboard shortcut for switching desktops (y or n)? " flag
 
---
+++ scripts/oh-my-posh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl -s https://ohmyposh.dev/install.sh | bash -s
+
+oh-my-posh font install hack
+
+cp files/jandedobbeleer.omp.json ~/.jandedobbeleer.omp.json
