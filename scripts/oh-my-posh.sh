4231d4109ce73beb2cf46132708da50a7b614f82
Alexey <alex@192.168.1.16>
2025-07-11 15:00:32 +0300

    sixth commit

--- scripts/oh-my-posh.sh
+++ scripts/oh-my-posh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -s https://ohmyposh.dev/install.sh | bash -s
 
-oh-my-posh font install hack
+oh-my-posh font install ubuntu
 
 cp files/jandedobbeleer.omp.json ~/.jandedobbeleer.omp.json
